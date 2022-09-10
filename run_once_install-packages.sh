d98d3569d5c5fa41bfe8479b075247b17824aa9d
kyeshmz <kyeshimizu@gmail.com>
2022-09-11 08:26:50 +0900

    Update run_once_install-packages.sh

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,6 +1,15 @@
 
 #!/bin/sh
 sudo softwareupdate --install-rosetta --agree-to-license
+
 brew bundle --global 
 
 flutter doctor --android-licenses 
+asdf plugin add ruby 
+asdf install ruby latest
+asdf plugin add nodejs
+asdf install nodejs lts
+asdf plugin add python
+sudo spctl --master-disable
+sudo xcode-select --switch /Applications/Xcode.app/Contents/Developer
+sudo xcodebuild -license
